e870ea0dafe30fcd1c4c9378fcee71d3c8f858f6
Maximilian Albert <maximilian.albert@soton.ac.uk>
2014-04-17 19:06:37 +0100

    Save output file in Data/ subdirectory.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 
-with open('output_file_containing_+_symbol.txt', 'w') as f:
+with open('Data/output_file_containing_+_symbol.txt', 'w') as f:
     f.write("This file will not be displayed or downloadable in the web interface.\n")
