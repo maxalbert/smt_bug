bc9e8c6b831965b472bfaa5bddcd1382da8e7ff0
Maximilian Albert <maximilian.albert@soton.ac.uk>
2014-04-17 19:10:40 +0100

    Also create a plot with a '+' symbol in the filename.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 
-with open('Data/output_file_containing_+_symbol.txt', 'w') as f:
+import matplotlib.pyplot as plt
+
+# Create a text file with a '+' sign in the filename
+with open('Data/text_file_with_+_symbol_in_filename.txt', 'w') as f:
     f.write("This file will not be displayed or downloadable in the web interface.\n")
+
+# Create a .png file with a '+' sign in the filename
+fig = plt.figure()
+ax = fig.gca()
+ax.plot([0, 1, 2, 3, 4], [1, 2, 1, 1.3, 1])
+fig.savefig('Data/png_file_with_+_symbol_in_filename.png')
